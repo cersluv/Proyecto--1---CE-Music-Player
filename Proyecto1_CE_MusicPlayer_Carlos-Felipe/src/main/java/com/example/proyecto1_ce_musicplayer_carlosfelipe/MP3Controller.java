84bd757c330e59586f30ad184274ea745fa169f3
cersluv <c.rodriguez@estudiantec.cr>
2022-09-17 11:43:47 -0600

    Primer Commit
    Se crearon las primeras dos ventanas, junto con sus respectivos iniciadores y controlladores. Se busca poder conectarlas, además de crear el sistema de usuarios y contraseñas.

---
+++ Proyecto1_CE_MusicPlayer_Carlos-Felipe/src/main/java/com/example/proyecto1_ce_musicplayer_carlosfelipe/MP3Controller.java
@@ -0,0 +1,29 @@
+package com.example.proyecto1_ce_musicplayer_carlosfelipe;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class MP3Controller {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="explote"
+    private Button explote; // Value injected by FXMLLoader
+
+    @FXML // fx:id="logout"
+    private Button logout; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert explote != null : "fx:id=\"explote\" was not injected: check your FXML file 'MP3Screen.fxml'.";
+        assert logout != null : "fx:id=\"logout\" was not injected: check your FXML file 'MP3Screen.fxml'.";
+
+    }
+
+}
---
+++ Proyecto1_CE_MusicPlayer_Carlos-Felipe/src/main/java/com/example/proyecto1_ce_musicplayer_carlosfelipe/Users.java
@@ -0,0 +1,5 @@
+package com.example.proyecto1_ce_musicplayer_carlosfelipe;
+
+public class Users extends  {
+
+}
